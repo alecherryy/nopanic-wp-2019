cb54eba58ef361547dba99b57d072a782582562a
Alessia Pizzoccheri <apizzoccheri@forumone.com>
2019-06-12 15:12:58 -0700

    set up wp

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_devalecherryy');
+define('DB_NAME', 'wp_nopanicdb');
 
 /** MySQL database username */
-define('DB_USER', 'wp_devalecherryy');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'wp_devalecherryy');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
